fff79f713d31882bb434ce7220da30ae0f78040c
JackDemeter <jack.demeter@hotmail.com>
2019-02-13 16:56:54 -0500

    Set up to accept our pretrained model, enter he name of the model in the file

--- vision_app.py
+++ vision_app.py
@@ -25,9 +25,9 @@ sys.path.append("..")
 
 # In[3]:
 
-from object_detection.utils import label_map_util
+from utils import label_map_util
 
-from object_detection.utils import visualization_utils as vis_util
+from utils import visualization_utils as vis_util
 
 
 # # Model preparation
@@ -41,36 +41,17 @@ from object_detection.utils import visualization_utils as vis_util
 # In[4]:
 
 # What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+MODEL_NAME = 'NAME OF MODEL'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('object_detection/data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join(MODEL_NAME, 'object-detection.pbtxt')
 
-NUM_CLASSES = 90
-
-
-# ## Download Model
-
-# In[5]:
-
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-  file_name = os.path.basename(file.name)
-  if 'frozen_inference_graph.pb' in file_name:
-    tar_file.extract(file, os.getcwd())
-
+NUM_CLASSES = 1
 
 # ## Load a (frozen) Tensorflow model into memory.
-
-# In[6]:
-
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
