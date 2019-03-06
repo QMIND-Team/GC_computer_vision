c3623e44a8a1640fddd40e47e4b6b3fb7318a3ef
bKolisnik <bKolisnik@gmail.com>
2019-03-06 18:43:43 -0500

    Gui and opencv integration

--- object_detection_gui.py
+++ object_detection_gui.py
@@ -1,18 +1,42 @@
+import cv2
 import sys
 from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, 
     QTextEdit, QGridLayout, QApplication, QHBoxLayout)
-#import cv2
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot, Qt
+
+
+
+
+class Thread(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
+
+
+
 
 class Example(QWidget):
     
     def __init__(self):
         super().__init__()
-        
+        self.title = "Grocery Checkout"
         self.initUI()
         
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+    	self.label.setPixmap(QPixmap.fromImage(image))
         
     def initUI(self):
-        
+        '''
         item = QLabel('Item')
         num = QLabel('Number')
         cost = QLabel('Unit Cost')
@@ -110,13 +134,17 @@ class Example(QWidget):
         self.setGeometry(300, 300, 600, 400)
         self.setWindowTitle('Grocery Checkout')    
         self.show()
+        '''
+        self.setWindowTitle(self.title)
+        self.setGeometry(300, 300, 600, 400)
         
-    def updateUI(self, items):
-        '''items: list of items where each index is the number seen'''
-
-        #for i in range(0,len(items):
-        #cap = cv2.VideoCapture(0)
-
+        self.label = QLabel(self)
+        self.label.move(280, 120)
+        self.label.resize(640, 480)
+        self.show()
+        th = Thread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
 
 
 
