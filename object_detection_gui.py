898d5366faa8463c6fbfd43d2abad63877db1cf2
bKolisnik <bKolisnik@gmail.com>
2019-03-07 22:11:29 -0500

    GUI CLEANN

--- object_detection_gui.py
+++ object_detection_gui.py
@@ -21,7 +21,7 @@ class Thread(QThread):
                 convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
                 p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
                 self.changePixmap.emit(p)
-                self.getValues.emit([0,0,0,0,0])
+                self.getValues.emit([2,0,3,0,0])
 
 
 
@@ -39,113 +39,74 @@ class Example(QWidget):
 
     @pyqtSlot(object)
     def getClasses(self, value):
-    	 print("List: {}".format(value))
+   	    print("List: {}".format(value))
+   	    PString=""
+   	    NumString=""
+   	    CostString=""
+   	    totalPrice=0
+   	    ApplePrice=1.50
+   	    OrangePrice=1.00
+   	    BananaPrice=1.25
+   	    appleTotal=0
+
+   	    bananaTotal=0
+   	    orangeTotal=0
+   	    #apple id 53
+   	    #banana id 52
+   	    #orange 55
+   	    TotalPrice=0
+   	    UnitCostString=""
+   	    Equals=""
+   	    Times=""
+
+   	    for i in range(0,len(value)):
+   	        if (value[i] > 0):
+   	            if i == 0:
+   	                PString+="Apple\n"
+   	                appleTotal=ApplePrice*value[i]
+   	                TotalPrice+=appleTotal
+   	                NumString+=str(value[i])+"\n"
+   	                CostString+=str(appleTotal)+"\n"
+   	                UnitCostString+=str(ApplePrice)+"\n"
+   	                Times+="X\n"
+   	                Equals+="=\n"
+
+   	            if i == 1:
+   	                PString+="Orange\n"
+   	                orangeTotal=OrangePrice*value[i]
+   	                TotalPrice+=orangeTotal
+   	                NumString+=str(value[i])+"\n"
+   	                CostString+=str(orangeTotal)+"\n"
+   	                UnitCostString+=str(OrangePrice)+"\n"
+   	                Times+="X\n"
+   	                Equals+="=\n"
+
+   	            if i == 2:
+   	                PString+="Banana\n"
+   	                bananaTotal=BananaPrice*value[i]
+   	                TotalPrice+=bananaTotal
+   	                NumString+=str(value[i])+"\n"
+   	                CostString+=str(bananaTotal)+"\n"
+   	                UnitCostString+=str(BananaPrice)+"\n"
+   	                Times+="X\n"
+   	                Equals+="=\n"
+
+   	    self.itemDyP.setText(PString)
+   	    self.numDyP.setText(NumString)
+   	    self.timesP.setText(Times)
+   	    self.costDyP.setText(UnitCostString)
+   	    self.equalsP.setText(Equals)
+
+   	    self.tcostDyP.setText(CostString)
+   	    self.gtcostDyP.setText(str(TotalPrice))
         
     def initUI(self):
-        '''
-        item = QLabel('Item')
-        num = QLabel('Number')
-        cost = QLabel('Unit Cost')
-        tcost = QLabel('Total Cost')
-
-        hori = QHBoxLayout()
-
-        grid = QGridLayout()
-        grid.setSpacing(10)
-
-        grid.addWidget(item, 0, 0)
-        grid.addWidget(num, 0, 1)
-        grid.addWidget(cost, 0, 3)
-        grid.addWidget(tcost, 0, 5)
-        
-        
-
-        
-        itemDyP = QLabel("Patel's Dal Tadka Lentil Curry")
-        numDyP = QLabel("0")
-        costDyP = QLabel("$3.50")
-        timesP = QLabel("X")
-        equalsP = QLabel("=")
-        tcostDyP = QLabel("0")
-        grid.addWidget(itemDyP, 1, 0, 3, 1)
-        grid.addWidget(numDyP, 1, 1, 3, 1)
-        grid.addWidget(timesP, 1, 2, 3, 1)
-        grid.addWidget(costDyP, 1, 3, 3, 1)
-        grid.addWidget(equalsP, 1, 4, 3, 1)
-        grid.addWidget(tcostDyP, 1, 5, 3, 1)
-
-        itemDyA = QLabel("Annie's Snicker Doodle")
-        numDyA = QLabel("0")
-        costDyA = QLabel("$3.50")
-        timesA = QLabel("X")
-        equalsA = QLabel("=")
-        tcostDyA = QLabel("0")
-        grid.addWidget(itemDyA, 2, 0, 3, 1)
-        grid.addWidget(numDyA, 2, 1, 3, 1)
-        grid.addWidget(timesA, 2, 2, 3, 1)
-        grid.addWidget(costDyA, 2, 3, 3, 1)
-        grid.addWidget(equalsA, 2, 4, 3, 1)
-        grid.addWidget(tcostDyA, 2, 5, 3, 1)
-
-        itemDyC = QLabel("Blue Diamond Nut Thins Almond Cheddar")
-        numDyC = QLabel("0")
-        costDyC = QLabel("$3.50")
-        timesC = QLabel("X")
-        equalsC = QLabel("=")
-        tcostDyC = QLabel("0")
-        grid.addWidget(itemDyC, 3, 0, 3, 1)
-        grid.addWidget(numDyC, 3, 1, 3, 1)
-        grid.addWidget(timesC, 3, 2, 3, 1)
-        grid.addWidget(costDyC, 3, 3, 3, 1)
-        grid.addWidget(equalsC, 3, 4, 3, 1)
-        grid.addWidget(tcostDyC, 3, 5, 3, 1)
-
-        itemDyS = QLabel("Blue Diamond Nut Thins Almond Seasalt")
-        numDyS = QLabel("0")
-        costDyS = QLabel("$3.50")
-        timesS = QLabel("X")
-        equalsS = QLabel("=")
-        tcostDyS = QLabel("0")
-        grid.addWidget(itemDyS, 4, 0, 3, 1)
-        grid.addWidget(numDyS, 4, 1, 3, 1)
-        grid.addWidget(timesS, 4, 2, 3, 1)
-        grid.addWidget(costDyS, 4, 3, 3, 1)
-        grid.addWidget(equalsS, 4, 4, 3, 1)
-        grid.addWidget(tcostDyS, 4, 5, 3, 1)
-
-        itemDyH = QLabel("Higgins and Burke Naturals")
-        numDyH = QLabel("0")
-        costDyH = QLabel("$3.50")
-        timesH = QLabel("X")
-        equalsH = QLabel("=")
-        tcostDyH = QLabel("0")
-        grid.addWidget(itemDyH, 5, 0, 3, 1)
-        grid.addWidget(numDyH, 5, 1, 3, 1)
-        grid.addWidget(timesH, 5, 2, 3, 1)
-        grid.addWidget(costDyH, 5, 3, 3, 1)
-        grid.addWidget(equalsH, 5, 4, 3, 1)
-        grid.addWidget(tcostDyH, 5, 5, 3, 1)
-        
-        itemDyH = QLabel("Final Total")
-        tcostDyF = QLabel("0")
-        grid.addWidget(itemDyH, 6, 0, 3, 1)
-        
-        grid.addWidget(tcostDyF, 6, 5, 3, 1)
-
-        #hori.addStretch(1)
-        hori.addLayout(grid)
-
-        self.setLayout(hori) 
-        
-        self.setGeometry(300, 300, 600, 400)
-        self.setWindowTitle('Grocery Checkout')    
-        self.show()
-        '''
 
         self.item = QLabel('Item')
         self.num = QLabel('Number')
         self.cost = QLabel('Unit Cost')
         self.tcost = QLabel('Total Cost')
+        self.gtcost = QLabel('Grand Total Cost')
 
         hori = QHBoxLayout()
         grid = QGridLayout()
@@ -155,14 +116,33 @@ class Example(QWidget):
         grid.addWidget(self.num, 0, 1)
         grid.addWidget(self.cost, 0, 3)
         grid.addWidget(self.tcost, 0, 5)
+        grid.addWidget(self.gtcost, 0, 6)
+        self.itemDyP = QLabel("")
+        self.numDyP = QLabel("")
+        self.costDyP = QLabel("")
+        self.timesP = QLabel("")
+        self.equalsP = QLabel("")
+        self.tcostDyP = QLabel("")
+        self.gtcostDyP = QLabel("")
+
+        grid.addWidget(self.itemDyP, 1, 0, 3, 1)
+        grid.addWidget(self.numDyP, 1, 1, 3, 1)
+        grid.addWidget(self.timesP, 1, 2, 3, 1)
+        grid.addWidget(self.costDyP, 1, 3, 3, 1)
+        grid.addWidget(self.equalsP, 1, 4, 3, 1)
+        grid.addWidget(self.tcostDyP, 1, 5, 3, 1)
+        grid.addWidget(self.gtcostDyP, 1, 6, 3, 1)
+        self.label = QLabel(self)
+        self.label.resize(300,300)
+        hori.addWidget(self.label)
         hori.addLayout(grid)
         self.setLayout(hori)
         self.setWindowTitle(self.title)
         self.setGeometry(300, 300, 600, 400)
         
-        self.label = QLabel(self)
-        self.label.move(280, 120)
-        self.label.resize(640, 480)
+        #self.label = QLabel(self)
+        #self.label.move(280, 120)
+        #self.label.resize(300,300)
 
         self.show()
         th = Thread(self)
